f7f0935c306d6236bc1a9bfc7b4348ef45670459
rohitmusti <rohit.musti.rm@gmail.com>
2021-02-14 11:01:02 -0500

    basic board set up

---
+++ script.js
@@ -0,0 +1,88 @@
+alert("hey");
+
+class Piece {
+  constructor(letter, number, type, team) {
+    this.letter = letter;
+    this.number = number;
+    this.type = type;
+    this.team = team;
+  }
+}
+
+class Board {
+  constructor() {
+    this.letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
+    this.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    this.game = {};
+    for (var i = 0; i < this.numbers.length; i++) {
+      for (var j = 0; j < this.letters.length; j++) {
+        this.game[this.letters[j] + this.numbers[i].toString()] = null;
+      }
+    }
+
+    let pattern = [
+      "rook",
+      "knight",
+      "bishop",
+      "queen",
+      "king",
+      "bishop",
+      "knight",
+      "rook",
+    ];
+
+    let redletters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    for (var i = 0; i < redletters.length; i++) {
+      this.game[redletters[i] + "2"] = new Piece(
+        redletters[i],
+        2,
+        "pawn",
+        "red"
+      );
+
+      this.game[redletters[i] + "1"] = new Piece(
+        redletters[i],
+        1,
+        pattern[i],
+        "red"
+      );
+    }
+
+    let whiteletters = ["l", "k", "j", "i", "d", "c", "b", "a"];
+    for (var i = 0; i < whiteletters.length; i++) {
+      this.game[whiteletters[i] + "7"] = new Piece(
+        whiteletters[i],
+        7,
+        "pawn",
+        "white"
+      );
+
+      this.game[whiteletters[i] + "8"] = new Piece(
+        whiteletters[i],
+        8,
+        pattern[i],
+        "white"
+      );
+    }
+
+    let blackletters = ["h", "g", "f", "e", "i", "j", "k", "l"];
+    for (var i = 0; i < blackletters.length; i++) {
+      this.game[blackletters[i] + "11"] = new Piece(
+        blackletters[i],
+        11,
+        "pawn",
+        "black"
+      );
+
+      this.game[blackletters[i] + "12"] = new Piece(
+        blackletters[i],
+        12,
+        pattern[i],
+        "black"
+      );
+    }
+  }
+}
+
+let p1 = new Board();
+console.log(p1);
